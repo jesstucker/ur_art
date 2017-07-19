09e6aeac0676f4d7afbcb7bb55c24928c8aaae3b
Jess Tucker <jess.tucker@gmail.com>
2017-07-18 22:20:39 -0700

    making django do things to s3 in a workable fashion lol

--- urart/models.py
+++ urart/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.conf import settings
+# from django.contrib.auth.models import User
 
 class Artwork(models.Model):
 	title = models.CharField(max_length=100)
 	url = models.CharField(max_length=500)
 	artist = models.ForeignKey(settings.AUTH_USER_MODEL)
+	# artist = models.ForeignKey(User, related_name='artist')
--- urart/views.py
+++ urart/views.py
@@ -1,9 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from urart.models import Artwork
+import boto
+from django.conf import settings
+from django.contrib.auth.models import User
 
 def home(request):
 	return render(request, 'home.html')
 
 def upload(request):
-	if request.method == 'POST':
-		artwork = Artwork()
+	if request.method == 'POST' and request.FILES['myfile']:
+		artwork = Artwork.objects.create(artist=request.user)
+		pic = request.FILES['myfile']
+		
+		conn = boto.connect_s3(aws_access_key_id=settings.AWS_ACCESS_KEY_ID, 
+			aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY, is_secure=False)
+		bucket = conn.get_bucket('urart')
+		key = bucket.new_key(str(artwork.id))
+		key.set_contents_from_file(pic)
+
+		artwork.url = key.generate_url(9999999)
+
+		# artwork.artist = request.user
+		# artwork.title = request.POST['title']
+		artwork.save()
+
+		return redirect('upload')
+	else:
+		return render(request, 'upload.html')
